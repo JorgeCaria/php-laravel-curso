ebfbde4f1401e5deaaec3f687252dba37b4eb765
JorgeDev <93055253+JorgeCaria@users.noreply.github.com>
2025-09-17 21:31:46 +0200

    dataType

---
+++ sintaxis/dataType.php
@@ -0,0 +1,15 @@
+<?php
+$number = 1;
+$decimal = 2.12;
+$name = "Esto es una cadena";
+$isFalse = false;
+$empty = null;
+
+//Casting
+// $number = (int)"1";
+
+$number = "1";
+
+echo $number;
+echo gettype($number);
+echo gettype($empty);
