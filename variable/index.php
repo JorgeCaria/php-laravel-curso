ecbe224b067d3647a1a38dc1f30d0d4dab6d1869
JorgeDev <93055253+JorgeCaria@users.noreply.github.com>
2025-09-17 21:15:39 +0200

    First commit

---
+++ variable/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// $a = 1;
+// $A = 2;
+
+// echo $a. " ";
+// echo $A;
+
+// $name = "Jorge";
+// echo $name;
+// $name = "Ana";
+// echo $name;
+
+// $name = false;
+// $name = 1.4;
+
+$age = 30;
+$name = "Ana";
+$message2 = "Más info";
+echo "Mi nombre es $name y tengo $age años ".$message2;
