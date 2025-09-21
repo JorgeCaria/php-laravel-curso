43cc215aaf206f5ad03ec14f7d50665a0ae4768e
JorgeDev <93055253+JorgeCaria@users.noreply.github.com>
2025-09-21 20:48:04 +0200

    estructura de control else elseif

--- sintaxis/if.php
+++ sintaxis/if.php
@@ -1,12 +1,14 @@
 <?php
 
-$age = 60;
+$age = 1;
 
-if($age ==18){
+if($age == 18){
     echo "Eres mayor de edad";
-}
-
-if($age > 18){
-    echo "Eres adulto mayor";
+}elseif($age > 18 && $age < 60){
+    echo "Eres mayor a 18";
+} elseif($age > 60){
+    echo "Eres una persona de tercera edad";
+}else {
+    echo "Eres menor de edad";
 }
 
